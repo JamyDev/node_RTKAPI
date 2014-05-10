107c449b6e8322c8ae6e7cfbf855eb4daa62f6ce
Jamy Timmermans <me@jamy.be>
2014-05-10 17:18:51 +0200

    Fixed reconnection etc :D

--- test/test-rtkconnection.js
+++ test/test-rtkconnection.js
@@ -8,26 +8,28 @@ exports.setUp = function (callback) {
 	ex.transform = null;
 	ex.server = net.createServer(function (con) {
 		ex.transform = new RTKTransform();
+		//ex.transform.removeAllListeners();
 		con.pipe(ex.transform);
     	ex.transform.on('data', function (p) {
     		if (p.method === 'enablePersistence')
     			con.write((new Packet(false, "0000000000000000000000000000000000000000000000000000000000000000", p.sessionID, "apiNotice", {}, "persistenceEnabled", null, 0x01, false)).compile());
     		else if (p.method === "getServerState")
     			con.write((new Packet(false, "0000000000000000000000000000000000000000000000000000000000000000", p.sessionID, "apiNotice", {}, JSON.stringify({state: "RUNNING"}), null, 0x01, false)).compile());
-    		else
-    			console.log("unknownp", p);
+    		//console.log(JSON.stringify(p));
+    		// else
+    		// 	console.log("unknownp", p);
     	});
 		
 	});
 	
-	ex.server.listen(25566);
+	ex.server.listen(255667);
 	callback();
 
 };
 
 exports.connectedEvent = function (test) {
 	test.expect(1);
-	var $api = new RTKConnection({host: "localhost", port: 25566, username: "user", password: "password", salt: "", secure: false}, function () {
+	var $api = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
 
 	});
 
@@ -38,28 +40,97 @@ exports.connectedEvent = function (test) {
 	});
 };
 
-exports.eventListeners = function (test) {
+exports.statusChange = function (test) {
 	test.expect(1);
-	setInterval(console.log, 2000, 1)
-	var $api = new RTKConnection({host: "localhost", port: 25566, username: "user", password: "password", salt: "", secure: false}, function () {
+	var $api = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
 		$api.enablePersistence();
-		
-		$api.on("com.drdanick.rtoolkit.event.WrapperConsoleOutputEvent", "asdf", function (con) {
-			test.ok(true);
-			test.done();
-		});
-		$api.call('consoleCommand', {}, "asdfasdf", function (resp) {
-			console.log("resp")
-		})
-		
 	});
+
+	$api.on("status", 'api', function () {
+		$api.removeAllListeners();
+		console.log($api.getStatus())
+		test.ok($api.status === 4);
+		test.done();
+	});
+};
+
+exports.reconnections = function (test) {
+	test.expect(10);
+	var $api = new RTKConnection({host: "localhost", port: 255666, username: "spacecp", password: "spacecp", salt: "E9tR0dw78FnQ3VY6qYVXhMOAGwYakhbrJBcZOQjltnVBQgRbCqZ1QJGkx2hqZa6", secure: false}, function () {
+		$api.enablePersistence();
+
+		setTimeout(function () {
+			$api.disconnect();
+			test.ok($api.getStatus() === 0);
+			setTimeout(function () {
+				$api.connect();
+			}, 2000)
+		}, 1000)
+	});
+
+	$api.on("status", 'api', function () {
+		// $api.removeAllListeners();
+		console.log($api.getStatus())
+		// test.ok($api.status === 4);
+		// test.done();
+	});
+};
+
+// exports.eventListeners = function (test) {
+// 	test.expect(1);
+// 	setInterval(console.log, 2000, 1);
+// 	var $api = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
+// 		$api.enablePersistence();
+		
+// 		$api.on("com.drdanick.rtoolkit.event.WrapperConsoleOutputEvent", "asdf", function (con) {
+// 			test.ok(true);
+// 			test.done();
+// 		});
+// 		$api.call('consoleCommand', {}, "asdfasdf", function (resp) {
+// 			console.log("resp");
+// 		});
+		
+// 	});
 	
 	
+// }
+
+exports.multipleServers = function  (test) {
+	var $api1 = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
+		$api1.enablePersistence();
+		test.ok("Done1")
+	});
+	var $api2 = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
+		$api2.enablePersistence();
+		test.ok("Done2")
+	});
+	var $api3 = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
+		$api3.enablePersistence();
+		test.ok("Done3")
+	});
+	var $api4 = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
+		$api4.enablePersistence();
+		test.ok("Done4")
+	});
+	var $api5 = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
+		$api5.enablePersistence();
+		test.ok("Done5")
+	});
+	var $api6 = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
+		$api6.enablePersistence();
+		test.ok("Done6")
+	});
+	var $api7 = new RTKConnection({host: "localhost", port: 255667, username: "user", password: "password", salt: "", secure: false}, function () {
+		$api7.enablePersistence();
+		test.ok("Done7")
+		test.done();
+	});
 }
 
 exports.tearDown = function (callback) {
 	this.server.close();
-	callback()
+	this.transform.removeAllListeners();
+	callback();
 }
 
 // exports.test = function (test) {
